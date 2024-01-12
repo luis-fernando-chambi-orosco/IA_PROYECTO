5a82ec2b4091f9146f0dc16086cdf03adcecd436
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2024-01-12 18:06:01 -0500

    actulizacion_proyecto

--- proyecto/predecir.py
+++ proyecto/predecir.py
@@ -2,6 +2,10 @@ import numpy as np
 from keras_preprocessing.image import load_img, img_to_array
 from keras.models import load_model
 
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
 # Tamaño de la imagen
 longitud, altura = 150, 150
 
@@ -14,25 +18,51 @@ cnn = load_model(modelo)
 cnn.load_weights(pesos)
 
 # Función para la predicción
-def predict(file):
-    x = load_img(file, target_size=(longitud, altura))  # Variable para cargar la imagen
-    x = img_to_array(x)  # La imagen se convierte en arreglo
-    x = np.expand_dims(x, axis=0)  # Se añade una dimensión extra en el eje cero para procesar la información sin problemas
-
-    arreglo = cnn.predict(x)  # Se procede a ejecutar la predicción, que devuelve un arreglo en dos dimensiones
-    resultado = arreglo[0]  # Se recupera el arreglo de la dimensión cero
-
-    respuesta = np.argmax(resultado)  # Se recupera el valor de índice más alto
-
-    # Se crea una condición if para clasificar como gato o perro
+def predecir_imagen(ruta):
+    x = load_img(ruta, target_size=(longitud, altura))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    
+    arreglo = cnn.predict(x)
+    resultado = arreglo[0]
+    respuesta = np.argmax(resultado)
+    
     if respuesta == 0:
-        print('Gato')
+        return 'Auto'
     elif respuesta == 1:
-        print('Perro')
-    return respuesta
+        return 'Moto'
 
-# Ejemplo de uso con una foto de otro animal
-# predict('gatodeprueba.jpg')
-predict('C:/Users/Aimer/Downloads/perrodeprueba.jpg')
-
+# Función para cargar y predecir una imagen seleccionada por el usuario
+def seleccionar_imagen():
+    ruta_imagen = filedialog.askopenfilename(title='Seleccionar Imagen', filetypes=[('Archivos de Imagen', '*.png;*.jpg;*.jpeg')])
+    
+    if ruta_imagen:
+        # Mostrar la imagen en la interfaz
+        img = Image.open(ruta_imagen)
+        img = img.resize((250, 250), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        panel_imagen.config(image=img)
+        panel_imagen.image = img
         
+        # Realizar la predicción
+        resultado_prediccion = predecir_imagen(ruta_imagen)
+        etiqueta_resultado.config(text=f'Resultado de la Predicción: {resultado_prediccion}')
+
+# Crear la interfaz
+app = tk.Tk()
+app.title('Clasificador de Autos y Motos')
+
+# Botón para seleccionar una imagen
+btn_seleccionar = tk.Button(app, text='Seleccionar Imagen', command=seleccionar_imagen)
+btn_seleccionar.pack(pady=10)
+
+# Panel para mostrar la imagen seleccionada
+panel_imagen = tk.Label(app)
+panel_imagen.pack()
+
+# Etiqueta para mostrar el resultado de la predicción
+etiqueta_resultado = tk.Label(app, text='Resultado de la Predicción: ')
+etiqueta_resultado.pack(pady=10)
+
+# Iniciar la interfaz
+app.mainloop()
